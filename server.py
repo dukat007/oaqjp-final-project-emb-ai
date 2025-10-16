df79338e2aa114943dcc7e9e335707e00e63b1a8
dukat007 <alexander.szpotowicz@gmx.de>
2025-10-16 17:22:05 -0400

    after static code analyis

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+''' This Module uses Flask deploy and run a webb app for emotion detection'''
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,6 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    '''This is the welcome / home page'''
     return render_template('index.html')
 
 @app.route('/emotionDetector')
@@ -15,16 +17,14 @@ def emotion_detection_fun():
     text_to_analyze = request.args.get('textToAnalyze') #retrieve parameter text
     res = emotion_detector(text_to_analyze) # apply function
     ## error handling ##
-    if res['dominant_emotion'] == None:
+    if res['dominant_emotion'] is None:
         return "Invalid text! Please try again!"
     ## output format ##
-    out_str = ("For the given statement, the system response is 'anger': " + str(res['anger']) 
-    + ", 'disgust': "  + str(res['disgust']) + ", 'fear': " + str(res['fear']) 
-    + ", 'joy': " + str(res['joy']) + " and 'sadness': " + str(res['sadness']) 
+    out_str = ("For the given statement, the system response is 'anger': " + str(res['anger'])
+    + ", 'disgust': "  + str(res['disgust']) + ", 'fear': " + str(res['fear'])
+    + ", 'joy': " + str(res['joy']) + " and 'sadness': " + str(res['sadness'])
     + ". The dominant emotion is " + str(res['dominant_emotion']) + ".")
     return out_str
 
 if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:5000
-    '''
     app.run(host="0.0.0.0", port=5000)
